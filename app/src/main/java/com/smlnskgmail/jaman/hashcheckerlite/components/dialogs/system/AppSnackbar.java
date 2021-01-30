735635dc80421b8288c0562e27899fe8ce6fb169
jayagrawal254 <77290619+jayagrawal254@users.noreply.github.com>
2021-01-30 20:54:03 +0530

    Update AppSnackbar.java

--- app/src/main/java/com/smlnskgmail/jaman/hashcheckerlite/components/dialogs/system/AppSnackbar.java
+++ app/src/main/java/com/smlnskgmail/jaman/hashcheckerlite/components/dialogs/system/AppSnackbar.java
@@ -59,13 +59,13 @@ public class AppSnackbar {
         );
 <<<<<<< HEAD
         private long backpressedTime;
-        @override
+        @Override
         protected void onCreate(Bundle SavedInstanceState){
                 super.onCreate(SavedInstanceState);
                 setContentView(R.layout.activity_main);
         }
 
-        @override
+        @Override
         public void onBackpressed()   {
                 if(backpressedTime + 2000 > System.currentTimeMillis()){
                      super.onBackpressed();
@@ -74,26 +74,6 @@ public class AppSnackbar {
                         Tost.makeText(getBaseContent(),"Press back again to exit",Tost.LENGTH_SHORT).show();
                 }
         }    
-=======
-        @override
-        Widget build(BuildContext context) {
-          return Scaffold(
-            ...
-            body: WillPopScope(child: getBody(), onWillPop: onWillPop),
-          );
-        }
-        
-        Future<bool> onWillPop() {
-            DateTime now = DateTime.now();
-            if (currentBackPressTime == null || 
-                now.difference(currentBackPressTime) > Duration(seconds: 2)) {
-              currentBackPressTime = now;
-              Fluttertoast.showToast(msg: exit_warning);
-              return Future.value(false);
-            }
-            return Future.value(true);
-          }
->>>>>>> my-branch
         snackbar.setActionTextColor(textColor);
         snackbar.getView().setBackground(
                 ContextCompat.getDrawable(
