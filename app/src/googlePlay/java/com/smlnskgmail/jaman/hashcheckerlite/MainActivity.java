acac57254080b66ced0f1b8bd24e2242531b7f73
Victor Pozdnyakov <victorius152@gmail.com>
2021-09-01 19:45:18 +0300

    2020-09-01 v. 1.0.8: deleted unused import
    1. Deleted:
    - unused import statement in app/src/googlePlay.../MainActivity.java

--- app/src/googlePlay/java/com/smlnskgmail/jaman/hashcheckerlite/MainActivity.java
+++ app/src/googlePlay/java/com/smlnskgmail/jaman/hashcheckerlite/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
 
